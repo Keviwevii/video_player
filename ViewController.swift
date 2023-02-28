7f2a08b3f19a1f9ab0520928eb14d75dcea535e9
Kevin Floyd <kfloydy@gmail.com>
2023-02-27 21:42:45 -0600

    completed the video app player

--- ViewController.swift
+++ ViewController.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo();
+    }
+    
+    private func playVideo() {
+        
+         guard let path = Bundle.main.path(forResource: "IMG_8353", ofType: "MOV") else {
+            
+             debugPrint("video.MOV not found")
+             
+             return
+        }
+        let player = AVPlayer(url:URL(fileURLWithPath: path))
+        
+        let playerController = AVPlayerViewController()
+        playerController.player=player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
 }
+    
+    
 
  
