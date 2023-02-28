f33c49d0dbd339f04bb197764a6ba62f21e27eea
Kevin Floyd <kfloydy@gmail.com>
2023-02-27 18:28:52 -0600

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Kevin Floyd on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
